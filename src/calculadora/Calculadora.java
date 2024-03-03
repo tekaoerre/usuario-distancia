e48122854d3f9145058a5ba91f9d0a8f15f25617
franj <franj@TKOR>
2024-03-03 20:07:21 +0100

    # usuario-distancia

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author franj
+ */
+/**
+ *  * Esta es la clase calculadora que proporciona métodos para realizar operaciones matemáticas básicas.
+ * @author franj
+ */
+
+public class Calculadora 
+{
+ /**
+     * Este es el constructor de la clase Calculadora.
+     */
+    public Calculadora() 
+    {
+        // Constructor
+    }
+    
+    /**
+     * Este método suma dos números enteros.
+     * 
+     * @param a el primer número entero a sumar
+     * @param b el segundo número entero a sumar
+     * @return la suma de los dos números
+     */
+    public int sumar(int a, int b) 
+    {
+        return a + b;
+    }
+    
+    /**
+     * Este método resta dos números enteros.
+     * 
+     * @param a el primer número entero (minuendo)
+     * @param b el segundo número entero (sustraendo)
+     * @return la resta de los dos números
+     */
+    public int restar(int a, int b) 
+    {
+        return a - b;
+    }
+}
